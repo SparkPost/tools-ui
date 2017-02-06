eb45fe8c249b0a60b36badf179427dbe083465c9
Jon Ambas <jon@jonambas.com>
2017-02-06 12:41:43 -0500

    FAD-4372 Create builder route, page, and nav link

---
+++ src/pages/builder/Builder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Builder extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Builder</h1>
+    );
+  }
+}
+
+export default Builder;
