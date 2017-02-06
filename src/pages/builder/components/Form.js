52d3442a4a4a3d21edeb2fc1640b9d3b597e5551
Jon Ambas <jon@jonambas.com>
2017-02-06 17:27:25 -0500

    FAD-4372 Connect spf form to redux-form

--- src/pages/builder/components/Form.js
+++ src/pages/builder/components/Form.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 
 class Form extends Component {
   constructor(props) {
@@ -6,19 +8,24 @@ class Form extends Component {
   }
 
   render() {
+    const { form } = this.props;
+    const domain = form && form.values ? form.values.domain : 'your domain';
+
     return (
         <div className='col-xs-12 col-md-10 col-lg-7'>
 
           <div className='panel'>
             <div className='panel__body'>
               <h4 className='marginBottom--xs'>Enter your domain</h4>
+              <Field name="domain" component="input" type="text" />
             </div>
           </div>
 
           <div className='panel'>
             <div className='panel__body'>
               <h4 className='marginBottom--xs'>MX Records</h4>
-              <p>Which domain’s MX records should be allowed to send mail for mydomain.com? Learn more about the MX mechanism.</p>
+              <p>Which domain’s MX records should be allowed to send mail for {domain}? Learn more about the MX mechanism.</p>
+              <Field name="addMX" component="input" type="text" />
             </div>
           </div>
 
@@ -55,4 +62,8 @@ class Form extends Component {
   }
 }
 
-export default Form;
+const mapStateToProps = ({ form }) => ({ form: form.spfBuilder });
+
+export default reduxForm({
+  form: 'spfBuilder' // a unique name for this form
+})(connect(mapStateToProps)(Form));
