4b28c99c0fbda3c30423c2da93d8700460b27c43
Jon Ambas <jon@jonambas.com>
2017-02-14 14:16:44 -0500

    FAD-4414 Clean up dkim header classname

--- src/pages/dkim/components/ResultDetailHeader.js
+++ src/pages/dkim/components/ResultDetailHeader.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import classnames from 'classnames';
 import { CopyPopover } from 'components/popover/Popover';
 import { ActionLink, SaveResultsLink } from 'components/button/Button';
 
 import './ResultDetailHeader.scss';
-
 
 const ResultDetailRow = ({ rows }) => (
   <span>
     {rows.map((row, key) => (
       <div className='dkimResultDetailHeader' key={key}>
         {row.map((value, key) => {
-          const classes = classnames({
-            'dkimResultDetailHeader__label': key === 0,
-            'dkimResultDetailHeader__value': key !== 0
-          });
-          return <span key={key} className={classes}>{value}</span>;
+          const type = key === 0 ? 'label' : 'value';
+          return <span key={key} className={`dkimResultDetailHeader__${type}`}>{value}</span>;
         })}
       </div>
     ))}
