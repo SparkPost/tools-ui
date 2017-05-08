a6e415b7352bcf226af69e3f68a7116f04d9579f
Rich Leland <rich.leland@sparkpost.com>
2017-05-08 12:52:38 -0400

    FAD-4815 update sign up URL

--- src/config/staging.js
+++ src/config/staging.js
@@ -2,5 +2,5 @@ export default {
   apiBase: 'https://api-staging.sparkpost.com/api/v1',
   appUrl: 'https://app-staging.sparkpost.com',
   logInUrl: 'https://app-staging.sparkpost.com/auth',
-  signUpUrl: 'https://app-staging.sparkpost.com/sign-up'
+  signUpUrl: 'https://app-staging.sparkpost.com/join'
 };
