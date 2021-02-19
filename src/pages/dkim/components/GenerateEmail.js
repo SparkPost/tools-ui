501ef5d2be0a6fbae7bc7d510600666bc9f334dc
Neal Stanard <nstanard@gmail.com>
2021-02-19 10:31:00 -0600

    FE-1350 - simple banner to explain we are performing an upgrade

--- src/pages/dkim/components/GenerateEmail.js
+++ src/pages/dkim/components/GenerateEmail.js
@@ -3,6 +3,9 @@ import { Button } from 'components/button/Button';
 
 export default (props) => (
   <span>
+    <div style={{ background: '#fa6423', color: '#fff', padding: '15px' }}>
+      Sorry, this tool is down for a scheduled maintenance right now. Please check back in a little bit.
+    </div>
     <div className='panel panel--accent'>
       <div className='panel__body paddingTop--xl paddingBottom--xl text--center'>
         <h4>To get started, click on the button below to generate an email address.</h4>
