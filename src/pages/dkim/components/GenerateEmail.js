9ebd6446d238babb134e34c667d9f754d2c6a1fd
Neal Stanard <nstanard@gmail.com>
2021-02-24 15:03:47 -0600

    FE-1350 - revert banner

--- src/pages/dkim/components/GenerateEmail.js
+++ src/pages/dkim/components/GenerateEmail.js
@@ -3,10 +3,6 @@ import { Button } from 'components/button/Button';
 
 export default (props) => (
   <span>
-    {/* will use for the major version upgrade in the following days/weeks FE-1350 */}
-    {/* <div style={{ background: '#fa6423', color: '#fff', padding: '15px' }}>
-      Sorry, this tool is down for a scheduled maintenance right now. Please check back in a little bit.
-    </div> */}
     <div className='panel panel--accent'>
       <div className='panel__body paddingTop--xl paddingBottom--xl text--center'>
         <h4>To get started, click on the button below to generate an email address.</h4>
