1e401dc71fa17f044b950c13ce2bfa321cedc910
Neal Stanard <nstanard@gmail.com>
2021-03-19 10:57:09 -0500

    add a generate email test

--- cypress/integration/DKIM/dkim.spec.js
+++ cypress/integration/DKIM/dkim.spec.js
@@ -14,4 +14,17 @@ describe('The DKIM inspector', () => {
   it('has the correct page header.', () => {
     cy.findByRole('heading', { name: 'DKIM Validator' }).should('be.visible');
   });
+
+  it('generates an email address.', () => {
+    cy.intercept({
+      method: 'POST',
+      url: '/api/v1/messaging-tools/validator-emails',
+    }).as('generateEmail');
+
+    cy.findByRole('button', { name: 'Generate Email Address' }).click();
+    cy.wait('@generateEmail');
+
+    // VIEW RESULTS -> is an anchor... so lets not worry about that for now
+    cy.findByRole('button', { name: 'Delete my email cookie and start over' }).should('be.visible');
+  });
 });
